cab54eca32201acdd150ad8f3321ea5e2e0b6dbe
Gian-Franco-L <GianXero@hotmail.com>
2023-11-17 16:29:53 -0300

    trying to solve /items refresh problems

--- components/FiltersCheckbox/FiltersCheckbox.tsx
+++ components/FiltersCheckbox/FiltersCheckbox.tsx
@@ -35,10 +35,6 @@ export default function FiltersCheckbox(){
     }
   }
 
-  useEffect(() =>{
-    router.push(`${pathname}?${searchFilter !== null ? `filter=${searchFilter}`: ''}${(checks[0].status && checks[1].status) ? '&itemTypes=Hogar|Niños' : checks[0].status ? '&itemTypes=Hogar' : checks[1].status ? '&itemTypes=Niños' : ''}`)
-  }, [checks])
-
   return(
     <>
       <h3>Filtrar por</h3>
