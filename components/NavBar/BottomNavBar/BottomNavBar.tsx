ea9205fd503da8c162aa29bace298553da2680f2
Gian-Franco-L <GianXero@hotmail.com>
2023-11-25 14:23:44 -0300

    added one less pixel to all the height words in product section

--- components/NavBar/BottomNavBar/BottomNavBar.tsx
+++ components/NavBar/BottomNavBar/BottomNavBar.tsx
@@ -22,7 +22,7 @@ export default function BottomNavBar(){
             </div>
             <ul className={`${BottomNavBarStyles.productsList} ${BottomNavBarStyles[productListStatus]}`}>
               <div className={BottomNavBarStyles.productSection}>
-                <Link href="/items/VueltaAlCole"><h5><li>Vuelta al cole</li></h5></Link>
+                <Link href="/items/VueltaAlCole"><p><li>Vuelta al cole</li></p></Link>
                 <Link href="/items/VueltaAlCole/Mochilas"><li>Mochilas</li></Link>
                 <span><Link href="/items/VueltaAlCole/Mochilas/Jardin"><li>- Jardín</li></Link></span>
                 <span><Link href="/items/VueltaAlCole/Mochilas/Primaria"><li>- Primaria</li></Link></span>
@@ -35,7 +35,7 @@ export default function BottomNavBar(){
                 <Link href="/items/VueltaAlCole/UniformeSC"><li>Uniforme San Cayetano</li></Link>
               </div>
               <div className={BottomNavBarStyles.productSection}>
-                <Link href="/items/Escritura"><h5><li>Escritura</li></h5></Link>
+                <Link href="/items/Escritura"><p><li>Escritura</li></p></Link>
                 <Link href="/items/Escritura/Lapices"><li>Lápices</li></Link>
                 <span><Link href="/items/Escritura/Lapices/Colores"><li>- Colores</li></Link></span>
                 <span><Link href="/items/Escritura/Lapices/Sacapuntas"><li>- Sacapuntas</li></Link></span>
@@ -52,10 +52,10 @@ export default function BottomNavBar(){
                 <Link href="/items/Escritura/Crayones"><li>Crayones</li></Link>
                 <Link href="/items/Escritura/Portaminas"><li>Portaminas</li></Link>
                 <span><Link href="/items/Escritura/Portaminas/Minas"><li>- Minas</li></Link></span>
-                <Link href="/items/LibrosParaColorear"><h5><li>Libros para colorear</li></h5></Link>
+                <Link href="/items/LibrosParaColorear"><p><li>Libros para colorear</li></p></Link>
               </div>
               <div className={BottomNavBarStyles.productSection}>
-                <Link href="/items/Carpetas"><h5><li>Carpetas</li></h5></Link>
+                <Link href="/items/Carpetas"><p><li>Carpetas</li></p></Link>
                 <Link href="/items/Carpetas/N3"><li>N°3</li></Link>
                 <Link href="/items/Carpetas/N5"><li>N°5</li></Link>
                 <Link href="/items/Carpetas/N6"><li>N°6</li></Link>
@@ -63,20 +63,20 @@ export default function BottomNavBar(){
                 <Link href="/items/Carpetas/3x40"><li>3x40</li></Link>
                 <Link href="/items/Carpetas/A4"><li>A4</li></Link>
                 <Link href="/items/Carpetas/3solapas"><li>3 Solapas</li></Link>
-                <Link href="/items/Cuadernos"><h5><li>Cuadernos</li></h5></Link>
+                <Link href="/items/Cuadernos"><p><li>Cuadernos</li></p></Link>
                 <Link href="/items/Cuadernos/A4"><li>A4</li></Link>
                 <Link href="/items/Cuadernos/A5"><li>A5</li></Link>
                 <Link href="/items/Cuadernos/A6"><li>A6</li></Link>
                 <Link href="/items/Cuadernos/Escolares"><li>Escolares</li></Link>
                 <Link href="/items/Cuadernos/Economicos"><li>Económicos</li></Link>
                 <Link href="/items/Cuadernos/CuadernoDibujo"><li>Cuaderno dibujo</li></Link>
-                <Link href="/items/Agendas"><li><h5>Agendas</h5></li></Link>
+                <Link href="/items/Agendas"><li><p>Agendas</p></li></Link>
               </div>
               <div className={BottomNavBarStyles.productSection}>
-                <Link href="/items/Calculadoras"><li><h5>Calculadoras</h5></li></Link>
+                <Link href="/items/Calculadoras"><li><p>Calculadoras</p></li></Link>
                 <Link href="/items/Calculadoras/Basicas"><li>Basicas</li></Link>
                 <Link href="/items/Calculadoras/Cientificas"><li>Cientificas</li></Link>
-                <Link href="/items/Arte"><li><h5>Arte</h5></li></Link>
+                <Link href="/items/Arte"><li><p>Arte</p></li></Link>
                 <Link href="/items/Arte/Temperas"><li>Témperas</li></Link>
                 <Link href="/items/Arte/Acuarelas"><li>Acuarelas</li></Link>
                 <Link href="/items/Arte/SetDibujo"><li>Set de dibujo</li></Link>
@@ -85,13 +85,13 @@ export default function BottomNavBar(){
                 <Link href="/items/Arte/Pinceles"><li>Pinceles</li></Link>
                 <Link href="/items/Arte/Paletas"><li>Paletas</li></Link>
                 <Link href="/items/Arte/Flautas"><li>Flautas</li></Link>
-                <Link href="/items/Geometria"><h5><li>Geometría</li></h5></Link>
+                <Link href="/items/Geometria"><p><li>Geometría</li></p></Link>
                 <Link href="/items/Geometria/Reglas"><li>Reglas</li></Link>
                 <Link href="/items/Geometria/Compas"><li>Compás</li></Link>
-                <Link href="/items/Adhesivos"><h5><li>Adhesivos</li></h5></Link>
+                <Link href="/items/Adhesivos"><p><li>Adhesivos</li></p></Link>
               </div>
               <div className={BottomNavBarStyles.productSection}>
-                <Link href="/items/Hojas"><h5><li>Hojas</li></h5></Link>
+                <Link href="/items/Hojas"><p><li>Hojas</li></p></Link>
                 <Link href="/items/Hojas/RepuestosEscolares"><li>Repuestos hojas escolares</li></Link>
                 <Link href="/items/Hojas/Separadores"><li>Separadores</li></Link>
                 <Link href="/items/Hojas/Etiquetas"><li>Etiquetas</li></Link>
@@ -99,18 +99,18 @@ export default function BottomNavBar(){
                 <Link href="/items/Hojas/Blocks"><li>Blocks</li></Link>
                 <span><Link href="/items/Hojas/Blocks/Recibos"><li>- Recibos</li></Link></span>
                 <Link href="/items/Hojas/Resmas"><li>Resmas</li></Link>
-                <Link href="/items/PapelesYCartones"><h5><li>Papeles y cartones</li></h5></Link>
+                <Link href="/items/PapelesYCartones"><p><li>Papeles y cartones</li></p></Link>
                 <Link href="/items"><li>Acetatos</li></Link>
-                <Link href="/items/Pizarras"><h5><li>Pizarras</li></h5></Link>
+                <Link href="/items/Pizarras"><p><li>Pizarras</li></p></Link>
                 <Link href="/items/Pizarras/Imanes"><li>Imanes</li></Link>
-                <Link href="/items/Tableros"><h5><li>Tableros</li></h5></Link>
-                <Link href="/items/Bastidores"><h5><li>Bastidores</li></h5></Link>
-                <Link href="/items/Masas"><h5><li>Masas</li></h5></Link>
-                <Link href="/items/GlobosTerraqueos"><h5><li>Globos terráqueos</li></h5></Link>
-                <Link href="/items/Cortantes"><h5><li>Cortantes</li></h5></Link>
-                <Link href="/items/BateriasYPilas"><h5><li>Baterias y pilas</li></h5></Link>
-                <Link href="/items/Fantasia"><h5><li>Fantasia</li></h5></Link>
-                <Link href="/items/Otros"><li><h5>Otros</h5></li></Link>
+                <Link href="/items/Tableros"><p><li>Tableros</li></p></Link>
+                <Link href="/items/Bastidores"><p><li>Bastidores</li></p></Link>
+                <Link href="/items/Masas"><p><li>Masas</li></p></Link>
+                <Link href="/items/GlobosTerraqueos"><p><li>Globos terráqueos</li></p></Link>
+                <Link href="/items/Cortantes"><p><li>Cortantes</li></p></Link>
+                <Link href="/items/BateriasYPilas"><p><li>Baterias y pilas</li></p></Link>
+                <Link href="/items/Fantasia"><p><li>Fantasia</li></p></Link>
+                <Link href="/items/Otros"><li><p>Otros</p></li></Link>
               </div>
             </ul>
           </li>
